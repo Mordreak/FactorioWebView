951a86b28c5302e3326115cacc780ca10e0380fa
Théodore Wittmann <thewit@live.fr>
2017-05-09 20:33:12 +0200

    script updated for time decrease

--- README.md
+++ README.md
@@ -20,12 +20,11 @@ Requirements:
           (your php.ini file is usually in /etc/php/apache2/php.ini)
 
 How to:
-        - Create a new user and database on your MySQL server, it will store the users able to access your web application.
         - Run the installation script as root called "installation.sh"
           --> "sudo ./installation.sh" make sure you have enough rights to run it. (chmod +x installation.sh)
         - Let the installation script guide you trough the steps.
         - Access http://factorio-web-view.local/config.php and check for warnings or errors to solve.
-        - You are done!
+        - You are done! Access your web manager at: http//factorio-web-view.local/
  
 To let other people access your web application:
         - Run "php bin/console fos:user:create" in your terminal. (this will create new users able to log in)
--- installation.sh
+++ installation.sh
@@ -5,17 +5,37 @@ set -e
 PURPLE='\033[0;35m'
 NC='\033[0m'
 
-echo -e "\n${PURPLE}INSTALLING DEPENDENCIES:"
+echo -e "\n${PURPLE}01 CONFIGURING MySQL:"
+echo -e "===============================${NC}\n"
+
+read -p "Enter your current MySQL root user: " dbUser
+read -s -p "Enter your current MySQL root password: " dbPasswd
+read -p "Enter a new Mysql username for this application (remember it for this script's third part): " dbNewUser
+read -s -p "Enter MySQL password for this application (remember it for this script's third part): " dbNewPasswd
+read -p "Enter a new name for your application database (remember it for this script's third part): " dbName
+
+if [ "$dbPasswd" = "" ]; then
+    DBCALL="mysql -u$dbUser"
+else
+    DBCALL="mysql -u$dbUser -p$dbPasswd"
+fi
+
+$DBCALL -e "CREATE DATABASE IF NOT EXISTS $dbName;"
+$DBCALL -e "CREATE USER '$dbNewUser'@'localhost' IDENTIFIED BY '$dbNewPasswd';"
+$DBCALL -e "GRANT ALL PRIVILEGES ON $dbName . * TO '$dbNewUser'@'localhost;"
+$DBCALL -e "FLUSH PRIVILEGES;"
+
+echo -e "\n${PURPLE}02 INSTALLING DEPENDENCIES:"
 echo -e "========================${NC}\n"
 
 php composer.phar install
 
-echo -e "\n${PURPLE}CONFIGURING SYMFONY & DOCTRINE:"
+echo -e "\n${PURPLE}03 CONFIGURING SYMFONY & DOCTRINE:"
 echo -e "===============================${NC}\n"
 
 php bin/console doctrine:schema:update --force
 
-echo -e "\n${PURPLE}CONFIGURING APACHE:"
+echo -e "\n${PURPLE}04 CONFIGURING APACHE:"
 echo -e "===================${NC}\n"
 
 cat vhost1.sample > /etc/apache2/sites-available/factorio-web-view.local.conf
@@ -26,7 +46,7 @@ a2ensite factorio-web-view.local.conf
 /etc/init.d/apache2 restart
 echo "127.0.0.1		factorio-web-view.local" >> /etc/hosts
 
-echo -e "\n${PURPLE}CONFIGURING PERMISSIONS:"
+echo -e "\n${PURPLE}05 CONFIGURING PERMISSIONS:"
 echo -e "========================${NC}\n"
 
 echo "chown -R www-data:www-data $PWD"
@@ -34,6 +54,11 @@ chown -R www-data:www-data $PWD
 
 echo "chmod -R 755 $PWD"
 chmod -R 755 $PWD
+
+echo -e "\n${PURPLE}05 CREATING NEW USER:"
+echo -e "========================${NC}\n"
+
+php bin/console fos:user:create
 
 echo -e "\n${PURPLE}DONE"
 echo -e "====${NC}\n"
