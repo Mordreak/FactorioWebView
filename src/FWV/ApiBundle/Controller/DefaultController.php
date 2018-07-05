57c5170618f13cdd16884ecbe56620cbe9ed1400
Théodore Wittmann <thewit@live.fr>
2018-07-05 22:09:40 +0200

    Added server stop

--- src/FWV/ApiBundle/Controller/DefaultController.php
+++ src/FWV/ApiBundle/Controller/DefaultController.php
@@ -18,11 +18,38 @@ class DefaultController extends Controller
         $result = array();
 
         try {
-            $result['isServerRunning'] = $manager->isServerRunning();
+            $result['isServerRunning'] = (boolean)$manager->isServerRunning();
         } catch (\Exception $e) {
             $result['isServerRunning'] = false;
         }
 
         return new JsonResponse($result);
+    }
+
+    /**
+     * Stops the server
+     *
+     * @return JsonResponse
+     */
+    public function stopServerAction()
+    {
+        $manager = $this->container->get('fwv_manager.helper_manager');
+        try {
+            if (!$manager->isServerRunning()) {
+                return new JsonResponse(array(
+                    'success' => false,
+                    'answer' => 'Server is not started'
+                ), 412);
+            }
+            $manager->stopServer();
+        } catch (\Exception $e) {
+            return new JsonResponse(array(
+                'success' => false,
+                'answer' => $e->getMessage()
+            ), 500);
+        }
+        return new JsonResponse(array(
+            'success' => true
+        ));
     }
 }
